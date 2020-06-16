f5ddbdb95d2ca2039b07dd597878dca8408cbda1
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-16 23:01:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,14 +13,13 @@ class Polygon{
  }
 }
 
-class Triangle extends Polygon {
+class Triangle extends Polygon{
+  get isValid(){
+    const points = this.points;
+    const firstSideValid = points[0] + points[1] > points[2]
+    const secondSideValid = points[0] + points[2] > points[1]
+    const thirdSideValid = points[1] + points[2] > points[0]
 
-  get isValid() {
-    if (!Array.isArray(this.sides)) return;
-    if (this.count !== 3) return;
-    let side1 = this.sides[0]
-    let side2 = this.sides[1]
-    let side3 = this.sides[2]
-    return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+    return firstSideValid && secondSideValid && thirdSideValid;
   }
 }
