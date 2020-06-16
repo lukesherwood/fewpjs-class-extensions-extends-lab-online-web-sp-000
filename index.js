fc21b115f80673fefddbd2c43fb824c9d709e3a3
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-16 22:59:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ class Polygon{
 }
 
 class Triangle extends Polygon {
-  get isValid() {
-    if (array.length == 3) {
-      if (array.[0])
+    get isValid() {
+      if (!Array.isArray(this.sides)) return;
+      if (this.count !== 3) return;
+      let side1 = this.sides[0]
+      let side2 = this.sides[1]
+      let side3 = this.sides[2]
+      return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
     }
-  }
 }
