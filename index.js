75cd33347ad86fb546f72b6cae3675e34b0d13c5
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-16 23:11:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Triangle extends Polygon{
 
 class Square extends Polygon {
   get isValid(){
+    if(this.countSides == )
     return (this.array[0] === this.array[1] === this.array[2] === this.array[3])
   }
   get area() {
