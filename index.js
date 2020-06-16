f66dcafa3c8ad038f69a3f6e668a60832fbbe88b
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-16 23:04:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,5 +25,9 @@ class Triangle extends Polygon{
 }
 
 class Square extends Polygon {
-  get 
+  get isValid(){
+    if (this.array[0] == this.array[1] == this.array[2] == this.array[3]){
+      return true
+    }
+  }
 }
